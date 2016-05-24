2ac3cfbfc1d7ec626f5f2c5f29a2fcfe4c91f319
Tom Lynam <tom.s.lynam@gmail.com>
2016-05-24 15:12:56 -0600

    added edit functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,10 +19,18 @@ class ItemsController < ApplicationController
 	end
   end
 
-
-
-
   def edit
+  	@item = Item.find(params[:id])
+  end
+
+  def update
+  	@item = Item.find(params[:id])
+
+  	if @item.update(item_params)
+  		redirect_to items_path
+  	else
+  		render :edit
+  	end
   end
 
 
